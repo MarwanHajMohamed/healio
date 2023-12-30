be160e309c651ba6db6ff2070c8f49f995f3633c
MarwanHajMohamed <111996420+MarwanHajMohamed@users.noreply.github.com>
2023-12-30 16:39:32 +0000

    Added prompt for user

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,22 +1,47 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "../css/main.css";
 
 export default function Main() {
   const [prompts, setPrompts] = useState([]);
   const [responses, setResponses] = useState([]);
+  const [text, setText] = useState("");
+  const textareaRef = useRef(null);
 
   const handlePrompts = (e) => {
     e.preventDefault();
-    setPrompts([...prompts, e]);
+    if (e.target.value === "") {
+    } else {
+      setPrompts([...prompts, e.target.value]);
+      setText("");
+      textareaRef.current.style.height = "33px";
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handlePrompts(e);
+    }
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+    textareaRef.current.style.height = "inherit";
+    textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
   };
 
   return (
     <div className="main-container">
       <div className="header">Healio</div>
-      <div className="response-container">
+      <div className="conversation-container">
         <div className="prompts">
           {prompts.map((prompt) => {
-            return <div className="prompt">{prompt}</div>;
+            return (
+              <div className="prompt-container">
+                <div className="prompt-title">You</div>
+                <div className="prompt">{prompt}</div>
+              </div>
+            );
           })}
         </div>
         <div className="responses">
@@ -25,13 +50,16 @@ export default function Main() {
           })}
         </div>
       </div>
-      <div className="prompt-container">
-        <form className="prompt-bar">
-          <input
+      <div className="user-prompt-container">
+        <form className="prompt-bar" onSubmit={handlePrompts}>
+          <textarea
+            rows="1"
             placeholder="How can I help?"
-            type="text"
-            onSubmit={(e) => handlePrompts(e.target.value)}
-          />
+            value={text}
+            ref={textareaRef}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          ></textarea>
         </form>
       </div>
     </div>
